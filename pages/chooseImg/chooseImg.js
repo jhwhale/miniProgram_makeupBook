cb7e3c458624ac5bcfdad042a166fbb7699b96c5
Jiang Haowen <haowen@weico.com>
2018-08-01 17:37:11 +0800

    图片选框功能完成
    完成图片选择、缩放、移动、裁剪功能

--- pages/chooseImg/chooseImg.js
+++ pages/chooseImg/chooseImg.js
@@ -1,32 +1,51 @@
 // pages/test/test.js
-// const app = getApp();
+/*
+TODOs:
+图片旋转
+*/
+
 const ctx = wx.createCanvasContext('cover-preview');
-var start_position = {};//移动图片时手指起始坐标
+var start_position;//移动图片时手指起始坐标
+var old_position;//上一次移动结束时手指的坐标
 var tempFilePath;//图片路径
-var tempWidth;//图片初始宽度
-var tempHeight;//图片初始高度
+var tempWidth;//源图片宽度
+var tempHeight;//源图片高度
+var showHeight = 0;//图片显示高度
+var showWidth = 0;//图片显示宽度
 var old_x = 0;//图片初始x坐标
 var old_y = 0;//图片初始y坐标
 var _touches = 1;//触屏的手指数
-var old_scale = 1;//原始放大倍数
-var new_scale = 1;//新的放大倍数
+var scale = 1;//缩放倍数
 var is_move = false;//是否移动
-var bg_url;
 var screenWidth = wx.getSystemInfoSync().screenWidth;
-var screenHeight = wx.getSystemInfoSync().screenHeight;
+var windowHeight = 0;//可用屏幕高度。无法通过wx.getSystemInfoSync()获得正确高度，所以在onReady()中获取
+var widthHeightRatio = 1;//图片宽高比
 
 Page({
   data: {
-    // hide_canvas: true,//绘图层显示控制变量
-    // imgSrc:''
+    btmHeight: 0,//底端蒙版的高度
   },
 
-
+  onReady: function () {//获取可用屏幕高度
+    wx.getSystemInfo({
+      success(res) {
+        windowHeight = res.windowHeight
+      }
+    });
+    this.setData({
+      btmHeight: windowHeight - windowHeight * 0.2 - screenWidth * 0.8//底端蒙版的高度=可用屏幕高度-顶部蒙版高度-图片选框高度
+    })
+  },
+  
   onLoad: function () {
+    this.chooseImg()
+  },
+
+  chooseImg: function(){
     var that = this;
     wx.chooseImage({
-      count: 1, // 默认9
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      count: 1, // 选择图片的数量
+      sizeType:  'compressed', // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
         tempFilePath = res.tempFilePaths[0];
@@ -35,13 +54,20 @@ Page({
           success: function (res) {
             tempWidth = res.width;
             tempHeight = res.height;
-            var showHeight = res.height / res.width * screenWidth;
-            console.log('show height: ',showHeight,' screen height: ',screenHeight)
-            if(showHeight<screenHeight*0.8){
-              ctx.drawImage(tempFilePath, 0, (screenHeight - showHeight) / 3, screenWidth, showHeight);//如果图片较短
-            }else{
-              ctx.drawImage(tempFilePath, 0, 0, screenWidth, showHeight);//如果图片较长
+            widthHeightRatio = tempWidth / tempHeight
+
+            if (widthHeightRatio <= 1) {//如果是竖图,适应屏幕宽度
+              showWidth = screenWidth
+              showHeight = screenWidth / widthHeightRatio
+              old_x = 0
+              old_y = (windowHeight - showHeight) / 2
+            } else {//如果是横图，适应选择框高度
+              showWidth = screenWidth * 0.8 * widthHeightRatio
+              showHeight = screenWidth * 0.8
+              old_x = (screenWidth - showWidth) / 2
+              old_y = windowHeight * 0.2//选框位于屏幕可用高度的20%，宽度的10%处，宽高均为屏幕宽度的80%
             }
+            ctx.drawImage(tempFilePath, old_x, old_y, showWidth, showHeight);
             ctx.draw();
           }
         })
@@ -49,39 +75,41 @@ Page({
     })
   },
 
-
   //监听手指触摸事件，并判断是移动还是缩放，并记录初始状态
   canvas_start: function (e) {
-    // console.log(e);
+    // console.log('start: ',e);
     var touches = e.touches.length;
-    if (touches == 1) {
-      _touches = 1;
-      start_position = { x: e.touches[0].x, y: e.touches[0].y, timeStamp: e.timeStamp }
-    } else if (touches == 2) {
+    if (touches == 2) {
       _touches = 2;
-      start_position = { x: e.touches[0].x, y: e.touches[0].y, x1: e.touches[1].x, y1: e.touches[1].y, timeStamp: e.timeStamp }
-    } else {
+      start_position = { x: e.touches[0].x, y: e.touches[0].y, x1: e.touches[1].x, y1: e.touches[1].y }
+    } else{
       _touches = 1;
-    }
+      start_position = { x: e.touches[0].x, y: e.touches[0].y }      
+    } 
+    old_position = start_position
   },
+
   //监听手指移动事件，并做出相应调整
   canvas_move: function (e) {
-    // console.log(e);
+    // console.log('move: ',e);
     var touches = e.touches.length;
-    if (_touches == 1 && e.timeStamp - start_position.timeStamp > 150) {
-      ctx.drawImage(tempFilePath, old_x + e.touches[0].x - start_position.x, old_y + e.touches[0].y - start_position.y, 375 * new_scale, tempHeight / tempWidth * 375 * new_scale);
+    if (_touches == 1) {
+      ctx.drawImage(tempFilePath, old_x + e.touches[0].x - start_position.x, old_y + e.touches[0].y - start_position.y, showWidth, showHeight);
       ctx.draw();
       is_move = true;
-    } else if (_touches == 2 && e.timeStamp - start_position.timeStamp > 150) {
-      var change_x = Math.abs(Math.abs(e.touches[0].x - e.touches[1].x) - Math.abs(start_position.x - start_position.x1));
-      var change_y = Math.abs(Math.abs(e.touches[0].y - e.touches[1].y) - Math.abs(start_position.y - start_position.y1));
+    } else if (_touches == 2) {
+      var change_x = Math.abs(Math.abs(e.touches[0].x - e.touches[1].x) - Math.abs(old_position.x - old_position.x1));//本次与上次缩放的长度差
+      var change_y = Math.abs(Math.abs(e.touches[0].y - e.touches[1].y) - Math.abs(old_position.y - old_position.y1));
       if (change_x - change_y > 10) {
-        old_scale = Math.abs(e.touches[0].x - e.touches[1].x) / Math.abs(start_position.x - start_position.x1);
+        scale = Math.abs(e.touches[0].x - e.touches[1].x) / Math.abs(old_position.x - old_position.x1);
       } else {
-        old_scale = Math.abs(e.touches[0].y - e.touches[1].y) / Math.abs(start_position.y - start_position.y1);
+        scale = Math.abs(e.touches[0].y - e.touches[1].y) / Math.abs(old_position.y - old_position.y1);
       }
-      ctx.drawImage(tempFilePath, old_x, old_y, 375 * old_scale * new_scale, tempHeight / tempWidth * 375 * old_scale * new_scale);
+      showWidth = showWidth * scale
+      showHeight = showHeight * scale
+      ctx.drawImage(tempFilePath, old_x, old_y, showWidth, showHeight);
       ctx.draw();
+      old_position = { x: e.touches[0].x, y: e.touches[0].y, x1: e.touches[1].x, y1: e.touches[1].y }
       is_move = true;
     } else {
       is_move = false;
@@ -89,30 +117,31 @@ Page({
   },
   //监听手指离开动作，并保存当前状态数据
   canvas_end: function (e) {
-    // console.log(e);
+    // console.log('end: ',e);
     if (_touches == 1 && is_move) {
       old_x = old_x + e.changedTouches[0].x - start_position.x;
       old_y = old_y + e.changedTouches[0].y - start_position.y;
     } else if (_touches == 2 && is_move) {
-      new_scale = old_scale * new_scale;
     }
 
   },
   //确定并上传背景图
   upload_bg: function () {
     var that = this;
-    // var screenWidth = wx.getSystemInfoSync().screenWidth;
-    // console.log(screenWidth);
     wx.canvasToTempFilePath({//把当前画布指定区域的内容导出生成指定大小的图片，并返回文件路径。
-      x: 0,
-      y: screenWidth / 750 * 400,
-      width: screenWidth,
-      height: screenWidth / 750 * 526,
-      destWidth: screenWidth,
-      destHeight: screenWidth / 750 * 526,
+      x: screenWidth/10,
+      y: windowHeight/5,
+      width: screenWidth*0.8,
+      height: screenWidth * 0.8,
+      destWidth: screenWidth * 0.8,
+      destHeight: screenWidth * 0.8,
       quality: 1,
       canvasId: 'cover-preview',
       success: function (res) {
+        // wx.saveImageToPhotosAlbum({
+        //   filePath: res.tempFilePath,
+        // })
+        
         var pages = getCurrentPages();
         var prevPage = pages[pages.length - 2];  //上一个页面
 
--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -9,19 +9,6 @@ TODOs:
 */
 const app = getApp()
 
-const ctx = wx.createCanvasContext('cover-preview');
-var start_position = {};//移动图片时手指起始坐标
-var tempFilePath;//图片路径
-var tempWidth;//图片初始宽度
-var tempHeight;//图片初始高度
-var old_x = 0;//图片初始x坐标
-var old_y = 0;//图片初始y坐标
-var _touches = 1;//触屏的手指数
-var old_scale = 1;//原始放大倍数
-var new_scale = 1;//新的放大倍数
-var is_move = false;//是否移动
-var bg_url;
-
 Page({
   /**
    * 页面的初始数据
@@ -159,19 +146,6 @@ Page({
     })
   },
 
-  // chooseImg: function () {
-  //   var that = this
-  //   wx.chooseImage({
-  //     sizeType: "original",//compressed
-  //     count: 1,
-  //     success: function (res) {
-  //       that.setData({
-  //         imgPath: res.tempFilePaths[0]
-  //       })
-  //     }
-  //   })
-  // },
-
   submit: function(e) {
     var id = this.data.id
     var objData = e.detail.value
@@ -198,10 +172,6 @@ Page({
       wx.navigateBack({
         delta: 1,
       })
-
-      // wx.reLaunch({
-      //   url: '../index/index',
-      // });
     }else{
       wx.showToast({
         title: '!!!未输入产品名称!!!',
@@ -232,126 +202,4 @@ Page({
     })
   }
 
-  // chooseImg: function () {
-  //   var that = this;
-  //   wx.showModal({
-  //     title: '提示',
-  //     content: '更改我的封面',
-  //     confirmColor: '#39bae8',
-  //     success: function (res) {
-  //       if (res.confirm) {
-
-
-  //         wx.chooseImage({
-  //           count: 1, // 默认9
-  //           sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-  //           sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-  //           success: function (res0) {
-
-  //             tempFilePath = res0.tempFilePaths[0];
-  //             that.setData({
-  //               hide_canvas: false,
-  //               // edit_url: tempFilePath
-  //             })
-  //             wx.getImageInfo({
-  //               src: tempFilePath,
-  //               success: function (res) {
-  //                 // console.log(res.width)
-  //                 // console.log(res.height)
-  //                 tempWidth = res.width;
-  //                 tempHeight = res.height;
-  //                 ctx.drawImage(tempFilePath, 0, 0, 375, res.height / res.width * 375);
-  //                 ctx.draw();
-  //               }
-  //             })
-
-  //           }
-  //         })
-  //       } else if (res.cancel) {
-  //         console.log('用户点击取消')
-  //       }
-  //     }
-  //   })
-  // },
-  // //监听手指触摸事件，并判断是移动还是缩放，并记录初始状态
-  // canvas_start: function (e) {
-  //   // console.log(e);
-  //   var touches = e.touches.length;
-  //   if (touches == 1) {
-  //     _touches = 1;
-  //     start_position = { x: e.touches[0].x, y: e.touches[0].y, timeStamp: e.timeStamp }
-  //   } else if (touches == 2) {
-  //     _touches = 2;
-  //     start_position = { x: e.touches[0].x, y: e.touches[0].y, x1: e.touches[1].x, y1: e.touches[1].y, timeStamp: e.timeStamp }
-  //   } else {
-  //     _touches = 1;
-  //   }
-  // },
-  // //监听手指移动事件，并做出相应调整
-  // canvas_move: function (e) {
-  //   // console.log(e);
-  //   var touches = e.touches.length;
-  //   if (_touches == 1 && e.timeStamp - start_position.timeStamp > 150) {
-  //     ctx.drawImage(tempFilePath, old_x + e.touches[0].x - start_position.x, old_y + e.touches[0].y - start_position.y, 375 * new_scale, tempHeight / tempWidth * 375 * new_scale);
-  //     ctx.draw();
-  //     is_move = true;
-  //   } else if (_touches == 2 && e.timeStamp - start_position.timeStamp > 150) {
-  //     var change_x = Math.abs(Math.abs(e.touches[0].x - e.touches[1].x) - Math.abs(start_position.x - start_position.x1));
-  //     var change_y = Math.abs(Math.abs(e.touches[0].y - e.touches[1].y) - Math.abs(start_position.y - start_position.y1));
-  //     if (change_x - change_y > 10) {
-  //       old_scale = Math.abs(e.touches[0].x - e.touches[1].x) / Math.abs(start_position.x - start_position.x1);
-  //     } else {
-  //       old_scale = Math.abs(e.touches[0].y - e.touches[1].y) / Math.abs(start_position.y - start_position.y1);
-  //     }
-  //     ctx.drawImage(tempFilePath, old_x, old_y, 375 * old_scale * new_scale, tempHeight / tempWidth * 375 * old_scale * new_scale);
-  //     ctx.draw();
-  //     is_move = true;
-  //   } else {
-  //     is_move = false;
-  //   }
-  // },
-  // //监听手指离开动作，并保存当前状态数据
-  // canvas_end: function (e) {
-  //   // console.log(e);
-  //   if (_touches == 1 && is_move) {
-  //     old_x = old_x + e.changedTouches[0].x - start_position.x;
-  //     old_y = old_y + e.changedTouches[0].y - start_position.y;
-  //   } else if (_touches == 2 && is_move) {
-  //     new_scale = old_scale * new_scale;
-  //   }
-
-  // },
-  // //确定并上传背景图
-  // upload_bg: function () {
-  //   var that = this;
-  //   var screenWidth = wx.getSystemInfoSync().screenWidth;
-  //   // console.log(screenWidth);
-  //   wx.canvasToTempFilePath({
-  //     x: 0,
-  //     y: screenWidth / 750 * 400,
-  //     width: screenWidth,
-  //     height: screenWidth / 750 * 526,
-  //     destWidth: screenWidth,
-  //     destHeight: screenWidth / 750 * 526,
-  //     quality: 1,
-  //     canvasId: 'cover-preview',
-  //     success: function (res) {
-  //       that.setData({
-  //         hide_canvas: true,
-  //         imgPath: res.tempFilePath
-  //       })
-  //       //res.tempFilePath即为生成的图片路径
-  //       console.log('upload_bg: ', res.tempFilePath)
-
-  //     }
-  //   })
-  // },
-  // //取消图片预览编辑
-  // cancel_croper: function () {
-  //   ctx.clearActions();
-  //   this.setData({
-  //     hide_canvas: true,
-  //     // edit_url: tempFilePath
-  //   })
-  // },
 })
